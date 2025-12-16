33c28f88b66a77a6f8b733175324f99fe59aa766
MikeSmid <122991896+MikeSmid@users.noreply.github.com>
2025-12-16 19:05:17 +0100

    Update MyWebViewClient.java

--- app/src/main/java/com/example/app/MyWebViewClient.java
+++ app/src/main/java/com/example/app/MyWebViewClient.java
@@ -12,7 +12,7 @@ class MyWebViewClient extends WebViewClient {
         String hostname;
 
         // YOUR HOSTNAME
-        hostname = "wwww.mikesmid.nl";
+        hostname = "mikesmid.nl";
 
         Uri uri = Uri.parse(url);
         if (url.startsWith("file:") || uri.getHost() != null && uri.getHost().endsWith(hostname)) {
